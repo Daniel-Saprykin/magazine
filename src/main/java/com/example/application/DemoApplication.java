47fef148e12edc852a6a7b1e6039ce29309ab18b
Даниил сапрыкин <d.v.saprykin@yandex.ru>
2024-07-17 16:01:01 +0300

    Release version 1.0.0

--- src/main/java/com/example/application/DemoApplication.java
+++ src/main/java/com/example/application/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// v1.0.0
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
